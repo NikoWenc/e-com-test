399cfc4d638151a562bfcc0fda578ebcc5f75ac7
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2026-02-22 11:22:19 +0800

    fix style of cart counter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,10 @@ import { ItemsContext } from "../context/ItemsContext";
 
 export default function Navbar() {
   const { cart } = useContext(ItemsContext);
-  const cartNumberStyle = cart.cartNumber <= 0 ? "sr-only" : "p-5 rounded-full";
+  const cartNumberStyle =
+    cart.cartNumber <= 0
+      ? "sr-only"
+      : "absolute w-5 h-5 text-[15px] font-bold -top-2.5 rounded-full bg-amber-300 ";
 
   return (
     <div className="flex py-5 px-30 bg-gray-500 w-screen h-20 fixed">
@@ -19,7 +22,7 @@ export default function Navbar() {
           <button className="text-2xl hover:text-yellow-500">Shop</button>
         </Link>
         <Link to="cart">
-          <button className="text-2xl hover:text-yellow-500">
+          <button className="text-2xl relative hover:text-yellow-500">
             Cart <span className={cartNumberStyle}>{cart.cartNumber}</span>
           </button>
         </Link>
