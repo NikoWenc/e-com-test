2edc37d3241ae65b9cd54b147671d2da26b6c327
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2026-04-17 01:37:19 +0800

    create GET/ route for API

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the E-commerce API");
+});
+
 app.use("/", router);
 
 app.listen(PORT, () => {
